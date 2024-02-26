6289695fc0028991c431a720ad90c262fd4a81f1
NatNichols <natalie.e.nichols@gmail.com>
2024-02-26 17:22:32 -0500

    add snake_index functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,10 @@
+# imports
 from django.urls import path
 from . import views
 
+# views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('snakes/', views.snake_index, name='snake-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,28 @@
+# imports
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Views:
+# temp data/models
+class Snake:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+snakes = [
+  Snake('Kaa', 'corn snake', 'Cutie.', 3),
+  Snake('Banana', 'python', 'Banan.', 0),
+  Snake('Noodle', 'hognose', 'Deadly cober.', 4),
+  Snake('Bonk', 'garter snake', 'Meows loudly.', 6)
+]
+
+# views
 def home(request):
   return HttpResponse('<h1>Hello, Snake Collector</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def snake_index(request):
+  return render(request, 'snakes/index.html', { 'snakes': snakes })
