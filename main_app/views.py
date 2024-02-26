5d863e1bdef0388acddd43aa11c5c47e1cc6d7b7
NatNichols <natalie.e.nichols@gmail.com>
2024-02-26 16:58:25 -0500

    define home URL and view function

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Views:
+def home(request):
+  return HttpResponse('<h1>Hello, Snake Collector</h1>')
