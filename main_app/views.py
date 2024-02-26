693ab95737936e777f56cc01f16703c8ac11b05e
NatNichols <natalie.e.nichols@gmail.com>
2024-02-26 17:03:51 -0500

    refactor about view function to use render, create templates

--- main_app/views.py
+++ main_app/views.py
@@ -6,4 +6,4 @@ def home(request):
   return HttpResponse('<h1>Hello, Snake Collector</h1>')
 
 def about(request):
-  return HttpResponse('<h1>About the Snake Collector</h1>')
+  return render(request, 'about.html')
