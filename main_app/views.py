3439b328a7ca595ecb1ce5b399b31ec03e5ace9f
NatNichols <natalie.e.nichols@gmail.com>
2024-02-27 13:05:17 -0500

    remove test Snake data

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,5 @@
 # imports
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# temp data/models
-class Snake:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-snakes = [
-  Snake('Kaa', 'corn snake', 'Cutie.', 3),
-  Snake('Banana', 'python', 'Banan.', 0),
-  Snake('Noodle', 'hognose', 'Deadly cober.', 4),
-  Snake('Bonk', 'garter snake', 'Meows loudly.', 6)
-]
 
 # views
 def home(request):
