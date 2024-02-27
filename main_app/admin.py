93791977a54ebf1d3a08943750abdd54709b5b94
NatNichols <natalie.e.nichols@gmail.com>
2024-02-27 18:07:21 -0500

    create superuser, import Snake model into main_app admin, register Snake model with admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Snake
 # Register your models here.
+admin.site.register(Snake)
+
+# git commit -m 'create superuser, import Snake model into main_app admin, register Snake model with admin'
