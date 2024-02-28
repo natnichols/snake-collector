d54696c7cb92bafcd10cbdc949c31a4c0842a840
NatNichols <natalie.e.nichols@gmail.com>
2024-02-28 13:19:53 -0500

    create and migrate Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
 # Create your models here.
 class Snake(models.Model):
   name = models.CharField(max_length=100)
@@ -12,4 +18,16 @@ class Snake(models.Model):
     return self.name
   
   def get_absolute_url(self):
-      return reverse('snake-detail', kwargs={'snake_id': self.id})
+    return reverse('snake-detail', kwargs={'snake_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField()
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  snake = models.ForeignKey(Snake, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
