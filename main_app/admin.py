7c2c86b6da5849e96388280a3be3c4cf340c2f4d
NatNichols <natalie.e.nichols@gmail.com>
2024-02-28 20:23:12 -0500

    import Hide model into main_app admin, register Hide model with admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Snake, Feeding
+from .models import Snake, Feeding, Hide
 
 # Register your models here.
 admin.site.register(Snake)
 admin.site.register(Feeding)
+admin.site.register(Hide)
