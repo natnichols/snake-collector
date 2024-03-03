c3e310dd1e8cb23da8e7fc983dd41f3e387f9dbe
NatNichols <natalie.e.nichols@gmail.com>
2024-03-02 21:30:38 -0500

    register Photo model in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Snake, Feeding, Hide
+from .models import Snake, Feeding, Hide, Photo
 
 # Register your models here.
 admin.site.register(Snake)
 admin.site.register(Feeding)
 admin.site.register(Hide)
+admin.site.register(Photo)
